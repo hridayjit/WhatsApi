1e2b379fe424430820ee78a84db3c5c9b3d71060
Hridayjit <hridayjit.kalita@yahoo.com>
2024-07-16 23:16:34 +0530

    latest one replaced

--- index.js
+++ index.js
@@ -16,18 +16,19 @@ let conn=mysql.createPool({
     host: "localhost",
     user: "root",
     password: "",
-    database: "campaign_db",
+    database: "db_connect",
     multipleStatements: true
 });
 
+//sendText
 app.get('/text/:id', (req, res) => {
     conn.getConnection((err, connection) => {
         if(err) throw err
         //console.log('params: '+req.params.id);
-        let sql = 'SELECT message FROM campaign_details WHERE is_deleted=0 AND cid=?';
-        let csql = 'SELECT id, phone FROM campaign_details_master WHERE is_deleted=0 AND status=0 AND cid=?';
+        var sql = 'SELECT message FROM campaign_details WHERE is_deleted=0 AND cid=?';
+        var csql = 'SELECT id, phone FROM campaign_details_master WHERE is_deleted=0 AND status=0 AND cid=?';
         
-        connection.query(sql+';'+csql, [req.params.id, req.params.id], (err, rows) => {
+        connection.query(sql+';'+csql, [req.params.id, req.params.id], async (err, rows) => {
             //connection.release();
             if(!err){
                 //let message = rows[0].message;
@@ -38,33 +39,66 @@ app.get('/text/:id', (req, res) => {
                 let ids=[];
                 contacts.forEach(element => {
                     phoneNos.push(element.phone);
-                });
-                contacts.forEach(element => {
                     ids.push(element.id);
                 });
-
-                console.log(message, phoneNos);
+                
+                //console.log(message, phoneNos);
                 const data = {
                     'message': message,
                     'phoneNos': phoneNos
                 };
-                
-                sendToWhatsapp(res, data, 5000, connection, ids);
+                //console.log(data);
+                await sendToWhatsapp(res, data, 5000, connection, ids);
             } else {
                 console.log(err);
             }
-            
             //console.log('The data from db are: \n', rows);
         });
+    });
+});
 
+//sendImage
+app.get('/image/:id', (req, res) => {
+    conn.getConnection((err, connection) => {
+        if(err) throw err
+        var sql = 'SELECT media, caption FROM campaign_details WHERE is_deleted=0 AND cid=?';
+        var csql = 'SELECT id, phone FROM campaign_details_master WHERE is_deleted=0 AND status=0 AND cid=?';
+        connection.query(sql+';'+csql, [req.params.id, req.params.id], async (err, rows) => {
+            if(!err){
+                let media = rows[0][0].media;
+                let caption = rows[0][0].caption;
+                let contacts = rows[1];
+                let phoneNos = [];
+                let ids = [];
+                contacts.forEach(element => {
+                    phoneNos.push(element.phone);
+                });
+                contacts.forEach(element => {
+                    ids.push(element.id);
+                });
+                const data = {
+                    'media': media,
+                    'caption': caption,
+                    'phoneNos': phoneNos
+                };
+                console.log(data);
+                await sendImageToWhatsapp(res, data, 5000, connection, ids);
+            }
+            else{
+                console.log(err);
+            }
+        });
     });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+
+//FUNCTIONS
+//async function (text)
 async function sendToWhatsapp(res, data, time, connection, ids){
     var updsql = 'UPDATE campaign_details_master SET status=1 WHERE is_deleted=0 and id=?';
-    const sent = await whatsapp.sendText(data, time);
+    const sent = await whatsapp.send('text',data, time);
     if(sent!=''){
         ids.forEach(element => {
             connection.query(updsql, [element], (err, rows) => {
@@ -78,7 +112,25 @@ async function sendToWhatsapp(res, data, time, connection, ids){
             });
         });
     }
-    //console.log(sent);
-    //res.send(sent);
 }
+//async function (image)
+async function sendImageToWhatsapp(res, data, time, connection, ids){
+    var updsql = 'UPDATE campaign_details_master SET status=1 WHERE is_deleted=0 and id=?';
+    const sent = await whatsapp.send('image',data, time);
+    if(sent!=''){
+        ids.forEach(element => {
+            connection.query(updsql, [element], (err, rows) => {
+                connection.release();
+                if(!err){
+                    res.send(sent);
+                }
+                else{
+                    console.log(err);
+                }
+            });
+        });
+    }
+}
+
+
 
--- whatsapp.js
+++ whatsapp.js
@@ -1,33 +1,65 @@
 const puppeteer = require('puppeteer');
-//const fs = require('fs');
+
 
 function delay(time){
     return new Promise(function(resolve){
         setTimeout(resolve, time);
     });
 }
-async function sendWhatsAppMessage(data, waitTime) {
-    const message = data.message;
-    let phoneNos=data.phoneNos;
 
+async function send(type, data, waitTime){
     //Launch a browser instance
     const browser = await puppeteer.launch({
         headless: false, //set to true if you want to run the script in  the background
         defaultViewport: null,
         userDataDir: './myUserDataDir'
     });
-
     //create a new page
     const page = await browser.newPage();
-
     //navigate to whatsapp Web
     await page.goto('https://web.whatsapp.com/');
+    //menu bar check
+    await page.waitForSelector('div._3OtEr[data-testid="menu-bar-menu"]');
+    await page.click('div._3OtEr[data-testid="menu-bar-menu"] div._3ndVb[role="button"]', {delay: 500});
+    await page.waitForSelector('li.Iaqxu.FCS6Q.jScby[data-testid="mi-logout menu-item"]');
+    await delay(1000);
+    //close the page
+    await page.close();
+    //close the browser
+    await browser.close();
+    let response='';
+    if(type==='text'){
+        response = await sendWhatsAppMessage(data, waitTime);
+    }
+    else if(type==='image'){
+       response = await sendWhatsappImage(data, waitTime);
+    }
+    else if(type==='file'){
+        response = await sendWhatsappFile(data, waitTime);
+    }
+    return response;
+}
 
+async function sendWhatsAppMessage(data, waitTime) {
+    const message = data.message;
+    let phoneNos=data.phoneNos;
+    //Launch a browser instance
+    const browser = await puppeteer.launch({
+        headless: true, //set to true if you want to run the script in  the background
+        defaultViewport: null,
+        userDataDir: './myUserDataDir'
+    });
+    //create a new page
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36');
+    //navigate to whatsapp Web
+    await page.goto('https://web.whatsapp.com/');
     for (const number of phoneNos) {
         await page.waitForSelector('div[title="Search input textbox"]');
         await page.type('div[title="Search input textbox"]', number, {delay: 0});
         const element = await page.waitForSelector('div.Er7QU');
         const value = await element.evaluate(el => el.textContent);
+        console.log(value);
         (value===number) ?  await page.keyboard.press('Enter') : console.log('error');
         await page.waitForSelector('div.fd365im1[title="Type a message"]');
         await page.type('div.fd365im1[title="Type a message"]', message, {delay: 0});
@@ -36,75 +68,93 @@ async function sendWhatsAppMessage(data, waitTime) {
         await ele.evaluate(elem => elem.textContent = '');
         await delay(waitTime);
     }
-
+    //close the page
     await page.close();
-
     //close the browser
     await browser.close();
     return 'Message: "'+message+'" sent to '+ phoneNos;
-
-    //await page.click('div[title="'+ number +'"]', {delay: 500});
+    //console.log();
 }
 
-async function sendWhatsappMedia(data, waitTime){
+async function sendWhatsappImage(data, waitTime){
     const media = data.media;
-    let phoneNos=data.phoneNos;
-
-    //Launch a browser instance
+    const caption = data.caption;
+    let phoneNos = data.phoneNos;
+    await delay(3000);
+    //launch a browser instance
     const browser = await puppeteer.launch({
-        headless: false, //set to true if you want to run the script in  the background
+        headless: true,
         defaultViewport: null,
         userDataDir: './myUserDataDir'
     });
-
     //create a new page
     const page = await browser.newPage();
-
-    //navigate to whatsapp Web
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36');
+    //navigate to whatsapp web
     await page.goto('https://web.whatsapp.com/');
+    //await delay(1000);
+    for (const number of phoneNos) {
+        await page.waitForSelector('div[title="Search input textbox"]');
+        await page.type('div[title="Search input textbox"]', number, {delay: 0});
+        const element = await page.waitForSelector('div.Er7QU');
+        const value = await element.evaluate(el => el.textContent);
+        console.log(value);
+        (value===number) ?  await page.keyboard.press('Enter') : console.log('error');
+        await page.waitForSelector('div._3ndVb[title="Attach"]');
+        await page.click('div._3ndVb[title="Attach"]', {delay: 0});
+        await page.waitForSelector('div._2ZCpJ');
+        await page.waitForSelector('div._8JAXG');
+        await page.waitForSelector('ul._3bcLp');
+        await page.waitForSelector('li.Iaqxu.FCS6Q._1LsXI[data-testid="mi-attach-media"]');
+        await page.hover('li.Iaqxu.FCS6Q._1LsXI[data-testid="mi-attach-media"]');
+        const elementHandle = await page.waitForSelector('li.Iaqxu.FCS6Q._1LsXI[data-testid="mi-attach-media"] button._1CGek input[accept="image/*,video/mp4,video/3gpp,video/quicktime"]');
+        elementHandle.uploadFile(media);
+        //add caption
+        await page.waitForSelector('p.selectable-text.copyable-text.iq0m558w');
+        await page.waitForSelector('p.selectable-text.copyable-text.iq0m558w');
+        await page.type('p.selectable-text.copyable-text.iq0m558w', caption, {delay: 0});
+        const captionEle = await page.waitForSelector('p.selectable-text.copyable-text.iq0m558w span.selectable-text.copyable-text');
+        const captionVal = await captionEle.evaluate(elem => elem.textContent);
+        (captionVal==caption) ? await page.click('span[data-testid="send"]', {delay: 0}) : console.log('Caption Error');
+        const repeatele = await page.waitForSelector('div[title="Search input textbox"]');
+        await repeatele.evaluate(elem => elem.textContent = '');
+        await delay(waitTime);
+    }
+    
+    //close the page
+    await page.close();
+    //close the browser
+    await browser.close();
+    /*const newdte = new Date();
+    const stop = newdte.getTime();
+    console.log((stop/1000));
+    console.log('Difference is: '+(stop/1000)-startSec);*/
+    return 'image sent to '+phoneNos;
+    //console.log();
+}
 
+//send File
+async function sendWhatsappFile(data, waitTime){
+    const file = data.file;
+    let caption = data.caption;
+    let phoneNos = data.phoneNos;
+    //Launch a browser instance
+    const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: null,
+        userDataDir: './myUserDataDir'
+    });
+    //create a new page
+    const page =await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36');
+    //navigate to whatsapp web
+    await page.goto('https://web.whatsapp.com/');
     for(const number of phoneNos){
         await page.waitForSelector('div[title="Search input textbox"]');
         await page.type('div[title="Search input textbox"]', number, {delay: 0});
         const element = await page.waitForSelector('div.Er7QU');
         const value = await element.evaluate(el => el.textContent);
-        (value===number) ?  await page.keyboard.press('Enter') : console.log('error');
-        await page.waitForSelector('div._3ndVb[title="Attach"]');
-        await page.click('div._3ndVb[title="Attach"]', {delay: 500});
-        await page.waitForSelector('div._2ZCpJ');
-        await page.waitForSelector('div._8JAXG');
-        await page.waitForSelector('ul._3bcLp');
-        await page.waitForSelector('li.Iaqxu.FCS6Q._1LsXI');
-        //await page.waitForSelector('button._1CGek');
-        await page.waitForSelector('button._1CGek');
-        await page.click('button._1CGek', {delay: 500});
-    }
-}
-
-async function sendWhatsappFile(data, waitTime){
-    const file = data.file;
-    let caption = data.caption;
-    let phoneNos = data.phoneNos;
-
-    //Launch a browser instance
-    const browser = await puppeteer.launch({
-        headless: false, //set to true if you want to run the script in  the background
-        defaultViewport: null,
-        userDataDir: './myUserDataDir'
-    });
-
-    //create a new page
-    const page = await browser.newPage();
-
-    //navigate to whatsapp Web
-    await page.goto('https://web.whatsapp.com/');
-    for (const number of phoneNos) {
-        //await page.waitForSelector('div[title="New chat"]');
-        
-        await page.waitForSelector('div.Er7QU[title="Search input textbox"]');
-        await page.type('div.Er7QU[title="Search input textbox"]', number, {delay: 0});
-        const element = await page.waitForSelector('div.Er7QU');
-        const value = await element.evaluate(el => el.textContent);
+        console.log(value);
         (value===number) ?  await page.keyboard.press('Enter') : console.log('error');
         await page.waitForSelector('div._3ndVb[title="Attach"]');
         await page.click('div._3ndVb[title="Attach"]', {delay: 0});
@@ -112,37 +162,44 @@ async function sendWhatsappFile(data, waitTime){
         await page.waitForSelector('div._8JAXG');
         await page.waitForSelector('ul._3bcLp');
         await page.waitForSelector('li.Iaqxu.FCS6Q._1LsXI[data-testid="mi-attach-document"]');
-        const elem = await page.waitForSelector('li.Iaqxu.FCS6Q._1LsXI[data-testid="mi-attach-document"] button._1CGek input[type="file"]');
-        await elem.uploadFile(file);
-        await delay(500);
-        //await page.waitForSelector('li.Iaqxu.FCS6Q._1LsXI[data-testid="mi-attach-document"] button._1CGek');
-        //await page.click('li.Iaqxu.FCS6Q._1LsXI[data-testid="mi-attach-document"] button._1CGek', {delay: 500});
+        await page.hover('li.Iaqxu.FCS6Q._1LsXI[data-testid="mi-attach-document"]');
+        const elementHandle = await page.waitForSelector('li.Iaqxu.FCS6Q._1LsXI[data-testid="mi-attach-document"] button._1CGek input[type="file"]');
+        elementHandle.uploadFile(file);
+        //add caption
+        await delay(2000);
         await page.waitForSelector('p.iq0m558w');
-        await page.type('p.iq0m558w', caption, {delay: 0});
+        await page.type('p.iq0m558w', caption, {delay: 500});
+        //const captionEle = await page.waitForSelector('p.iq0m558w span.selectable-text.copyable-text');
+        //const captionVal = await captionEle.evaluate(elem => elem.textContent);
+        //(captionVal==caption) ? await page.click('div[aria-label="send"]', {delay: 0}) : console.log('Caption Error');
         await page.click('div[aria-label="Send"]', {delay: 0});
+        const repeatele = await page.waitForSelector('div[title="Search input textbox"]');
+        await repeatele.evaluate(elem => elem.textContent = '');
         await delay(waitTime);
     }
     await page.close();
-    
     //close the browser
     await browser.close();
-    return 'File is sent to '+ phoneNos;
+    /*const newdte = new Date();
+    const stop = newdte.getTime();
+    console.log((stop/1000));*/
+    return 'File is sent to '+phoneNos;
+    //console.log('File is sent to '+phoneNos);
 }
 
 module.exports = {
-    sendText: sendWhatsAppMessage,
-    sendMedia: sendWhatsappMedia,
-    sendFile: sendWhatsappFile
+    //sendText: sendWhatsAppMessage,
+    //sendImage: sendWhatsappImage,
+    //sendFile: sendWhatsappFile
+    send: send
 }
 
 //sendWhatsAppMessage(data);
-const data = {
-    'file': 'files/upload1.pdf',
-    'caption': 'The pdf is posted',
-    'phoneNos': ['7783047335', '7783047335']
+
+/*const data = {
+    'file': './files/screenrecording.mov',
+    'caption': 'link this pic',
+    'phoneNos': ['917783047335', '917783047335']
 };
-sendWhatsappFile(data, 5000);
 
-
-
-
+send('file', data, 5000);*/
